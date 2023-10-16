6ee371fed92e840578b8aaf9b25e2da93a7ca77b
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-10-16 14:39:32 +0200

    Add index for slug

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,19 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer, index } from "drizzle-orm/sqlite-core";
 import { relations, sql } from "drizzle-orm";
 
-export const rooms = sqliteTable("rooms", {
-	id: integer("id").primaryKey(),
-	title: text("title"),
-	description: text("description"),
-	slug: text("slug").unique(),
-	userId: text("user_id").notNull(),
-});
+export const rooms = sqliteTable(
+	"rooms",
+	{
+		id: integer("id").primaryKey(),
+		title: text("title"),
+		description: text("description"),
+		slug: text("slug").unique(),
+		userId: text("user_id").notNull(),
+	},
+	(table) => ({
+		slugIndex: index("slug_idx").on(table.slug),
+	}),
+);
 
 export const roomsRelations = relations(rooms, ({ many }) => ({
 	messages: many(messages),
